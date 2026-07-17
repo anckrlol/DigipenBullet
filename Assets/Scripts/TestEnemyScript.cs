b6658e9c90834c3309011e11c6ae8c1eb7a6df58
anckrlol <leggoleggo1211@gmail.com>
2026-07-17 19:12:05 -0400

    oh my god animations dear god help me
    i dont like you unity

--- Assets/Scripts/TestEnemyScript.cs
+++ Assets/Scripts/TestEnemyScript.cs
@@ -32,7 +32,7 @@ public class TestEnemyScript : MonoBehaviour
     }
     
     IEnumerator AttackFinished(){
-        yield return new WaitForSeconds(16);
+        yield return new WaitForSeconds(200);
         turnManager.playerTurnState?.Invoke(true);
     }
     
