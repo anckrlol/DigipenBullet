679e2d63bd4cd5465cd61a22d0a4c8a4e8907bf3
anckrlol <leggoleggo1211@gmail.com>
2026-07-17 19:20:01 -0400

    fix fix fix

--- Assets/Scripts/TestEnemyScript.cs
+++ Assets/Scripts/TestEnemyScript.cs
@@ -32,7 +32,7 @@ public class TestEnemyScript : MonoBehaviour
     }
     
     IEnumerator AttackFinished(){
-        yield return new WaitForSeconds(200);
+        yield return new WaitForSeconds(12);
         turnManager.playerTurnState?.Invoke(true);
     }
     
